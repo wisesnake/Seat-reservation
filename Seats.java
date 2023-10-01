987d2957bb3f64d751698d5dee5ef6e3c5550bcc
DESKTOP-TJGVGLB\wises <wisesnake0618@gmail.com>
2023-10-01 11:50:53 +0900

    main 메뉴 틀 짜놓은 코드

---
+++ Seats.java
@@ -0,0 +1,45 @@
+package booktheseat;
+
+public class Seats {
+	private int seatNo; // 좌석 번호
+	private int bookedCustNo; // 예약 번호
+	private boolean booked; // 예약 여부
+
+	public Seats(int seatNo, int bookedCustNo) {
+		this.seatNo = seatNo;
+		this.bookedCustNo = bookedCustNo;
+		this.booked = false;
+	}
+	
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+
+	public void setBookedCustNo(int bookedCustNo) {
+		this.bookedCustNo = bookedCustNo;
+	}
+
+	public void setBooked(boolean booked) {
+		this.booked = booked;
+	}
+	
+	public void initSample(int seatNo, int bookedCustNo) {
+		this.seatNo = seatNo;
+		this.bookedCustNo = bookedCustNo;
+		this.booked = true;
+	}
+
+
+	public int getSeatNo() {
+		return seatNo;
+	}
+
+	public int getBookedCustNo() {
+		return bookedCustNo;
+	}
+
+	public boolean isBooked() {
+		return booked;
+	}
+
+}
